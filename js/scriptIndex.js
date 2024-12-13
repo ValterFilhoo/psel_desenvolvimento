803c6daa8878774f759808f1f279adeecc9cc402
Valter Filho <valter.filho7@aluno.enova.educacao.ba.gov.br>
2024-12-13 16:32:29 -0300

    Implementação do bloco de código js que realiza a Verificação de Segurança no formulário.

--- js/scriptIndex.js
+++ js/scriptIndex.js
@@ -5,19 +5,51 @@ mobileMenu.addEventListener('click', () => {
     navList.classList.toggle('menuAtivo');
 });
 
-const primeiroValor = document.getElementById('primeiroValor').value;
-const segundoValor = document.getElementById('segundoValor').value;
-const inputResultado = document.getElementById('inputResultado').value;
+
+const primeiroValor = document.getElementById('primeiroValor');
+const segundoValor = document.getElementById('segundoValor');
+const inputResultado = document.getElementById('inputResultado');
 const operacao = document.getElementById('operacao');
+
+const resultadoVerificacao = document.getElementById('resultadoVerificacao');
 
 function verificaSoma() {
 
-    if (primeiroValor && segundoValor) {
-        const resultado = primeiroValor + segundoValor;
+    const valor1 = parseInt(primeiroValor.textContent);
+    const valor2 = parseInt(segundoValor.textContent);
+    let valorDoUsuario = inputResultado.value.trim();
+
+    if (valorDoUsuario === "") {
+        resultadoVerificacao.innerText = "Erro. Insira o valor da soma na Verificação de Segurança.";
+        resultadoVerificacao.style.display = 'flex';
+        return
+    }
+
+    valorDoUsuario = parseInt(inputResultado.value);
+
     
-        operacao.innerText = resultado;
+    if (!isNaN(valor1) && !isNaN(valor2)) {
+
+        const resultado = valor1 + valor2;
+
+
+        if (resultado === valorDoUsuario) {
+
+            resultadoVerificacao.innerText = `O resultado da soma da verificação de segurança é: ${resultado}.`;
+
+            resultadoVerificacao.innerText += " Você acertou!";
+
+        } else {
+
+            resultadoVerificacao.innerText = "A soma está incorreta. Tente novamente.";
+
+        }
+
+        resultadoVerificacao.style.display = 'Flex';
+
+    } else {
+        console.log("Erro nos valores:", valor1, valor2);
     }
 
 }
-
 
